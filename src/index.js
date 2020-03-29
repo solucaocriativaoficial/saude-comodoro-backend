a38efde7226da77d2c44e5a27c86435a2f68ca1c
brunolgb <brunodscolado@gmail.com>
2020-03-29 15:19:00 -0400

    add o cors na rota inicial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const Router = require('./routes')
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(Router)
 
